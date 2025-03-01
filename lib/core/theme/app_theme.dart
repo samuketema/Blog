0249bde30e35a9f5e56d412dabd04a0cc1e062ff
Samuel <samuelketema01@gmail.com>
2025-03-01 18:46:24 +0300

    input decoroation theme is added.

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -2,7 +2,18 @@ import 'package:blog/core/theme/app_pallete.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
+  static OutlineInputBorder _border({Color color =AppPallete.borderColor}) => OutlineInputBorder(
+        borderSide: BorderSide(
+          color: color,
+          width: 2
+        )
+      );
   static final darkThemeMode = ThemeData.dark().copyWith(
-    scaffoldBackgroundColor: AppPallete.backgroundColor
+    scaffoldBackgroundColor: AppPallete.backgroundColor,
+    inputDecorationTheme:InputDecorationTheme(
+      contentPadding: EdgeInsets.all(7),
+      enabledBorder:_border(),
+      focusedBorder: _border(color: AppPallete.gradient2)
+    )
   );
 }
