dd127cbfba0d2295ca80a8eb3c61901a72e90561
Samuel <samuelketema01@gmail.com>
2025-03-01 18:47:26 +0300

    Text field modified

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -1,3 +1,4 @@
+import 'package:blog/features/auth/presentation/widgets/auth_field.dart';
 import 'package:flutter/material.dart';
 
 class SignupPage extends StatefulWidget {
@@ -10,6 +11,21 @@ class SignupPage extends StatefulWidget {
 class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Sign Up." , style:TextStyle(fontWeight: FontWeight.bold,fontSize: 27),),
+            SizedBox(height: 20,),
+            AuthField(hintText: 'email',),
+            SizedBox(height: 20,),
+            AuthField(hintText: 'Password',),
+            
+          ],
+        ),
+      ),
+    );
   }
 }
