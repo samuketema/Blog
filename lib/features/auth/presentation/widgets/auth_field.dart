e7b2ef88605d4320b8088b29d60a7e858b70f2ba
Samuel <samuelketema01@gmail.com>
2025-03-03 13:09:03 +0300

    Controller and validator properties are added.

--- lib/features/auth/presentation/widgets/auth_field.dart
+++ lib/features/auth/presentation/widgets/auth_field.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 class AuthField extends StatelessWidget {
-  String? hintText;
-   AuthField({super.key, this.hintText});
+  final TextEditingController controller;
+  final String? hintText;
+  const AuthField({super.key, this.hintText, required this.controller});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      validator: (value) {
+        if (value!.isEmpty) {
+          return '$hintText is missing!';
+        }
+        return null;
+      },
+      controller: controller,
       decoration: InputDecoration(
         hintText:hintText )
     );
