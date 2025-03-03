5d774e174b7110a6f0cdcd9d4f3f7d66c50cfb94
Samuel <samuelketema01@gmail.com>
2025-03-03 13:08:22 +0300

    Controller for the text fields.

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -1,3 +1,4 @@
+import 'package:blog/core/theme/app_pallete.dart';
 import 'package:blog/features/auth/presentation/widgets/auth_field.dart';
 import 'package:blog/features/auth/presentation/widgets/raised_button.dart';
 import 'package:flutter/material.dart';
@@ -10,24 +11,51 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Sign Up." , style:TextStyle(fontWeight: FontWeight.bold,fontSize: 27),),
-            SizedBox(height: 20,),
-            AuthField(hintText: 'email',),
-            SizedBox(height: 20,),
-            AuthField(hintText: 'Password',),
-            SizedBox(height: 20,),
-            RaisedButton(text: "Sign Up",)
-
-            
-          ],
+        child: Form(
+          key:formKey ,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Sign Up.",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 27),
+              ),
+              SizedBox(height:20),
+              AuthField(hintText: 'name',
+              controller: nameController,),
+              SizedBox(height: 20),
+              AuthField(hintText: 'email',controller: emailController,),
+              SizedBox(height: 20),
+              AuthField(hintText: 'Password',controller: passwordController,),
+              SizedBox(height: 20),
+              RaisedButton(text: "Sign Up"),
+              SizedBox(height: 10),
+              RichText(
+                text: TextSpan(
+                  text: "Already have an account? ",
+                  style: Theme.of(context).textTheme.titleMedium,
+                  children: [
+                    TextSpan(
+                      text: "Sign In",
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                        color: AppPallete.gradient2,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
