3cb0b80beba4ce073cd76ec532edabb734c12105
Samuel <samuelketema01@gmail.com>
2025-03-07 01:34:45 +0300

    value assigned for message

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 class Failure {
   String messageText;
-  Failure({
-    required this.messageText,
-  });
+  Failure([ this.messageText = 'An unexpected error has occured',]
+    
+  );
 }
