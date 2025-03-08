96e7e6378c0cbbf87c349719744ccadf2d50b1a5
Samuel <samuelketema01@gmail.com>
2025-03-08 15:30:03 +0300

    Application secrets stored.

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,4 @@
+class AppSecrets {
+  static const url = 'https://fsykedvivnqmgouuwclh.supabase.co' ;
+  static const annonkey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzeWtlZHZpdm5xbWdvdXV3Y2xoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQwODc4OTAsImV4cCI6MjA0OTY2Mzg5MH0.guhKbWUrKQkynb757FnPkcebMlXgmMEF4oX_r2CSd7U ';
+}
