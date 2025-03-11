9fa38574ba83b4805eed6c0b8184b547e941775f
Samuel <samuelketema01@gmail.com>
2025-03-11 13:39:25 +0300

    Custom exception class.

---
+++ lib/core/error/exception.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class ServerException implements Exception {
+  final String message;
+  ServerException(
+     this.message,
+  );
+}
