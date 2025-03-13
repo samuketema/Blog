2a12289ddaee4296e5981ec6a192b2cad46a27d1
Samuel <samuelketema01@gmail.com>
2025-03-13 22:55:36 +0300

    AuthBloc provider added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:blog/core/secrets/app_secrets.dart';
 import 'package:blog/core/theme/app_theme.dart';
+import 'package:blog/features/auth/data/datasource/auth_remote_datasource.dart';
+import 'package:blog/features/auth/data/repository/auth_repository_imple.dart';
+import 'package:blog/features/auth/domain/usecases/user_sign_up.dart';
+import 'package:blog/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:blog/features/auth/presentation/pages/signup_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -11,7 +15,25 @@ void main() async {
     url: AppSecrets.url,
     anonKey: AppSecrets.annonkey,
   );
-  runApp(MultiBlocProvider(providers: [], child: const MyApp()));
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create:
+              (_) => AuthBloc(
+                userSignUp: UserSignUp(
+                  AuthRepositoryImple(
+                    remoteDataSource: AuthRemoteDatasourceImpl(
+                      supabaseClient: supabase.client,
+                    ),
+                  ),
+                ),
+              ),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
