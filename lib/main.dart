658ea8877eaf7e78080047c51d168cf9e00a1f2b
Samuel <samuelketema01@gmail.com>
2025-03-13 22:37:35 +0300

    Wrapping with provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,16 @@ import 'package:blog/core/secrets/app_secrets.dart';
 import 'package:blog/core/theme/app_theme.dart';
 import 'package:blog/features/auth/presentation/pages/signup_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final supabase = await Supabase.initialize(url: AppSecrets.url, anonKey: AppSecrets.annonkey);
-  runApp(const MyApp());
+  final supabase = await Supabase.initialize(
+    url: AppSecrets.url,
+    anonKey: AppSecrets.annonkey,
+  );
+  runApp(MultiBlocProvider(providers: [], child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -22,4 +26,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
